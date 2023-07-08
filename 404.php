e5b886fab6c1d6535fcd2aa4a8847c6d19e762bf
lisadebona <lisaqdebona@gmail.com>
2023-07-08 01:20:20 -0400

    Built 404page and sitemap page

--- 404.php
+++ 404.php
@@ -7,33 +7,26 @@
  * @package bellaworks
  */
 
-$title = get_field("title404","option");
-$content = get_field("content404","option");
 get_header(); ?>
 
-<div id="primary" class="content-area default-template page404">
-  <main id="main" class="site-main wrapper">
+<div id="primary" class="content-area-full generic-layout page404-content">
+  <main id="main" class="site-main" role="main">
 
-    <header class="entry-title">
-      <div class="wrapper">
-        <div class="small-title">404 ERROR</div>
-        <h1 class="page-title"><span><?php esc_html_e( 'Page Not Found!', 'bellaworks' ); ?> <!-- <span class="smiley"> ¯\_(ツ)_/¯ </span> --></span></h1>
+      <div class="titlediv">
+        <div class="wrapper"><h1 class="page-title"><?php esc_html_e( 'Page Not Found!', 'bellaworks' ); ?></h1></div>
       </div>
-    </header>
-
-    <section class="entry-content content404">
-      <div class="wrapper">
-        <div class="innerText">
+      
+      
+      <div class="entry-content contentDiv">
+        <div class="wrapper">
           <p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below.', 'bellaworks' ); ?></p>
           <div id="sitemap-wrap">
             <?php wp_nav_menu( array( 'theme_location' => 'sitemap', 'menu_id' => 'sitemap','container_class'=>'sitemap-links') ); ?>
           </div>
         </div>
       </div>
-    </section>
-
-  </main><!-- #main -->
-</div><!-- #primary -->
+  </main>
+</div>
 
 <?php
 get_footer();
